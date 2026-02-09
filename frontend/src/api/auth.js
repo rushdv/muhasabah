8319211185dcd75207b85740256944b129797261
Md Shihab Shahriar Rashu <md.shihabshahriarrashu@gmail.com>
2026-02-10 02:07:00 +0600

    fixed api base url in client.js for deployment

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,13 +1,13 @@
 import api from "./client";
 
 export const login = async (email, password) => {
-  const res = await api.post("/auth/login", { email, password });
+  const res = await api.post("/api/auth/login", { email, password });
   localStorage.setItem("token", res.data.access_token);
   return res.data;
 };
 
 export const signup = async (username, email, password) => {
-  const res = await api.post("/auth/signup", { username, email, password });
+  const res = await api.post("/api/auth/signup", { username, email, password });
   localStorage.setItem("token", res.data.access_token);
   return res.data;
 };
